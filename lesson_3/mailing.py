10a016f96b2966230d7a24a58219233de6c9d34b
Anna-77-88-99 <anna.shashkina99@gmail.com>
2024-06-15 00:29:51 +0300

    lesson 3 first commit

---
+++ lesson_3/mailing.py
@@ -0,0 +1,6 @@
+class Mailing:
+    def __init__(self, to_address, from_address, cost, track):
+        self.ta = to_address
+        self.fa = from_address
+        self.ct = cost
+        self.tck = track
